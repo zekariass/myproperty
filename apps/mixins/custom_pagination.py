06a271d2daedb3229ce69a3e26abe653f09cc7b0
Zekarias <znegese@gmail.com>
2023-07-13 16:32:20 +0100

    Some change

--- apps/mixins/custom_pagination.py
+++ apps/mixins/custom_pagination.py
@@ -3,7 +3,12 @@ from apps.system.models import SystemParameter
 
 
 class GeneralCustomPagination(LimitOffsetPagination):
-    _page_limit = (
-        SystemParameter.objects.filter(name="NUMBER_OF_ITEMS_PER_PAGE").first().value
-    )
+    try:
+        _page_limit = (
+            SystemParameter.objects.filter(name="NUMBER_OF_ITEMS_PER_PAGE")
+            .first()
+            .value
+        )
+    except Exception:
+        _page_limit = 5
     default_limit = int(_page_limit) if _page_limit else 3
