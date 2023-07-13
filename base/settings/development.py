253126d6ea257d2c88761b587e68675b35552233
Zekarias <znegese@gmail.com>
2023-07-13 18:01:15 +0100

    all app migrations regenerated

--- base/settings/development.py
+++ base/settings/development.py
@@ -6,18 +6,15 @@ DEBUG = True
 
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # },
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "grinmove",
-        "PASSWORD": "Grinmove23",
-        "HOST": "myproperty.cgtjrid2hzqu.eu-west-2.rds.amazonaws.com",
-        "PORT": "5432",
-    }
+    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     "NAME": "postgres",
+    #     "USER": "grinmove",
+    #     "PASSWORD": "Grinmove23",
+    #     "HOST": "myproperty.cgtjrid2hzqu.eu-west-2.rds.amazonaws.com",
+    #     "PORT": "5432",
+    # }
 }
 
 
